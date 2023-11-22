2f165cd286d69b1023208b208029d23f7c4addc6
AlexSabu <sabualex333@gmail.com>
2023-11-23 00:12:56 +0530

    first addition

---
+++ script.js
@@ -0,0 +1,13 @@
+/*let arr=[1,2,3,4,5],name="alex";
+console.log(arr);
+
+foo(arr[1])
+function foo(num){
+    console.log(num*num);
+}
+
+foo2()
+function foo2(){
+    console.log("hello"+name);
+}
+*/
